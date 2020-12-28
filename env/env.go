d862aa40a653627d1f417c2a4516e1fbe4f801a0
Alexander Babai <aliaksandr.babai@gmail.com>
2020-12-29 01:04:35 +0300

    docs(env): cover funcs

--- env/env.go
+++ env/env.go
@@ -5,6 +5,7 @@ import (
 	"os"
 )
 
+// GetEnv returns the value of the environment variable named by the key or default value if missing
 func GetEnv(key, defaultValue string) string {
 	if key == "" {
 		return defaultValue
@@ -17,6 +18,7 @@ func GetEnv(key, defaultValue string) string {
 	return defaultValue
 }
 
+// GetEnv returns the value of the environment variable named by the key or panic if missing
 func GetRequiredEnv(key string) string {
 	if key == "" {
 		panic("empty key argument")
