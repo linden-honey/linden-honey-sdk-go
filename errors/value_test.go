8beabf4487da6bce3fdd3ec361dc8912badf4fe4
Alexander Babai <aliaksandr.babai@gmail.com>
2021-10-06 01:10:47 +0300

    test(errors.value): fix cases

--- errors/value_test.go
+++ errors/value_test.go
@@ -2,11 +2,43 @@ package errors
 
 import (
 	"errors"
-	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 )
+
+func TestNewInvalidValueError(t *testing.T) {
+	type args struct {
+		key string
+		err error
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "ok",
+			args: args{
+				key: "Title",
+				err: errors.New("title should be in camelcase"),
+			},
+			want: "'Title' has invalid value: title should be in camelcase",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			rq := require.New(t)
+
+			got := NewInvalidValueError(tt.args.key, tt.args.err)
+			rq.NotNil(got)
+			rq.Equal(
+				tt.want,
+				got.Error(),
+			)
+		})
+	}
+}
 
 func TestNewRequiredValueError(t *testing.T) {
 	type args struct {
@@ -15,64 +47,25 @@ func TestNewRequiredValueError(t *testing.T) {
 	tests := []struct {
 		name string
 		args args
+		want string
 	}{
 		{
 			name: "ok",
 			args: args{
 				key: "Title",
 			},
+			want: "'Title' has invalid value: required value is missing",
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			rq := require.New(t)
 
-			err := NewRequiredValueError(tt.args.key)
-			rq.NotNil(err)
-			rq.True(errors.Is(err, ErrRequiredValue))
-			rq.EqualError(
-				err,
-				fmt.Errorf(
-					"'%s' has invalid value: %w",
-					tt.args.key,
-					ErrRequiredValue,
-				).Error(),
-			)
-		})
-	}
-}
-
-func TestNewInvalidValueError(t *testing.T) {
-	type args struct {
-		key string
-		err error
-	}
-	tests := []struct {
-		name    string
-		args    args
-		wantErr bool
-	}{
-		{
-			name: "ok",
-			args: args{
-				key: "Title",
-				err: errors.New("title should be in camelcase"),
-			},
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			rq := require.New(t)
-
-			err := NewInvalidValueError(tt.args.key, tt.args.err)
-			rq.NotNil(err)
-			rq.EqualError(
-				err,
-				fmt.Errorf(
-					"'%s' has invalid value: %w",
-					tt.args.key,
-					tt.args.err,
-				).Error(),
+			got := NewRequiredValueError(tt.args.key)
+			rq.NotNil(got)
+			rq.Equal(
+				tt.want,
+				got.Error(),
 			)
 		})
 	}
