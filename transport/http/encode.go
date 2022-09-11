954c7e4179bc8eb256e7c44b491ba05f0ba8c4ef
Alexander Babai <aliaksandr.babai@gmail.com>
2022-09-12 00:16:41 +0300

    feat(transport.http): encode json response

---
+++ transport/http/encode.go
@@ -0,0 +1,18 @@
+package http
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+// EncodeJSONResponse serializes the response as a JSON object to the ResponseWriter with appropriate status code
+func EncodeJSONResponse(w http.ResponseWriter, code int, response interface{}) error {
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	w.WriteHeader(code)
+
+	if code == http.StatusNoContent {
+		return nil
+	}
+
+	return json.NewEncoder(w).Encode(response)
+}
