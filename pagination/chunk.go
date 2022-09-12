ba44800c13ac4060036c4e9325c164c0ae20beb3
Alexander Babai <aliaksandr.babai@gmail.com>
2022-09-12 03:03:14 +0300

    refactor(pagination): clean up

--- pagination/chunk.go
+++
@@ -1,7 +0,0 @@
-package pagination
-
-// Chunk represents a block of paginated data
-type Chunk[T any] struct {
-	Data []T `json:"data"`
-	Pageable
-}
--- pagination/pagination.go
+++ pagination/pagination.go
@@ -14,3 +14,16 @@ const (
 
 // Sort represents a mapping SortBy -> SortOrder
 type Sort map[SortBy]SortOrder
+
+// Pageable represents a type for pagination support
+type Pageable struct {
+	Limit  int  `json:"limit"`
+	Offset int  `json:"offset"`
+	Sort   Sort `json:"sort"`
+}
+
+// Chunk represents a block of paginated data
+type Chunk[T any] struct {
+	Data []T `json:"data"`
+	Pageable
+}
