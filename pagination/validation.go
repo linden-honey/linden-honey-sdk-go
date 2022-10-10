1b40d518745b0ee23fef83812cdb1784ba6e7333
Alexander Babai <aliaksandr.babai@gmail.com>
2022-10-11 02:55:55 +0300

    docs: validation comments

--- pagination/validation.go
+++ pagination/validation.go
@@ -6,7 +6,7 @@ import (
 	sdkerrors "github.com/linden-honey/linden-honey-sdk-go/errors"
 )
 
-// Validate validates a SortBy and returns an error if validation is failed
+// Validate a SortBy and returns an error if validation is failed
 func (sb SortBy) Validate() error {
 	if sb == "" {
 		return sdkerrors.ErrEmptyValue
@@ -15,7 +15,7 @@ func (sb SortBy) Validate() error {
 	return nil
 }
 
-// Validate validates a SortOrder and returns an error if validation is failed
+// Validate a SortOrder and returns an error if validation is failed
 func (so SortOrder) Validate() error {
 	if so < Ascending || so > Descending {
 		return fmt.Errorf("should have one of the following values %v, %v, %v", Ascending, Normal, Descending)
@@ -24,7 +24,7 @@ func (so SortOrder) Validate() error {
 	return nil
 }
 
-// Validate validates a Sort and returns an error if validation is failed
+// Validate a Sort and returns an error if validation is failed
 func (s Sort) Validate() error {
 	for sb, so := range s {
 		if err := sb.Validate(); err != nil {
@@ -38,7 +38,7 @@ func (s Sort) Validate() error {
 	return nil
 }
 
-// Validate validates a Pageable and returns an error if validation is failed
+// Validate a Pageable and returns an error if validation is failed
 func (p Pageable) Validate() error {
 	if p.Limit < 0 {
 		return sdkerrors.NewInvalidValueError("Limit", sdkerrors.ErrNegativeValue)
@@ -53,7 +53,7 @@ func (p Pageable) Validate() error {
 	return nil
 }
 
-// Validate validates a Chunk[T] and returns an error if validation is failed
+// Validate a Chunk[T] and returns an error if validation is failed
 func (c Chunk[T]) Validate() error {
 	if err := c.Pageable.Validate(); err != nil {
 		return err
