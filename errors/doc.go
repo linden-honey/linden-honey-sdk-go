c6b6adfee047c98000d55df7c9dc244fd96ae860
Alexander Babai <aliaksandr.babai@gmail.com>
2022-11-24 17:47:07 +0300

    docs(errors): add package comment

---
+++ errors/doc.go
@@ -0,0 +1,5 @@
+// Package errors provides a general way to create and manage errors.
+//
+// It contains a set of predefined error variables for typical use cases
+// and a set of error factory functions to create errors with specific details.
+package errors
