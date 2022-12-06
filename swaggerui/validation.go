a5a738cd65c714e9f1e2300682e97629b5c358b3
Alexander Babai <aliaksandr.babai@gmail.com>
2022-12-06 23:08:56 +0300

    refactor: use sdkerrors alias

--- swaggerui/validation.go
+++ swaggerui/validation.go
@@ -3,41 +3,41 @@ package swaggerui
 import (
 	"strings"
 
-	"github.com/linden-honey/linden-honey-sdk-go/errors"
+	sdkerrors "github.com/linden-honey/linden-honey-sdk-go/errors"
 )
 
 // Validate validates a [Config] and returns an error if validation is failed.
 func (cfg Config) Validate() error {
 	if strings.TrimSpace(cfg.Title) == "" {
-		return errors.NewInvalidValueError("Title", errors.ErrEmptyValue)
+		return sdkerrors.NewInvalidValueError("Title", sdkerrors.ErrEmptyValue)
 	}
 
 	if strings.TrimSpace(cfg.SpecURL) == "" {
-		return errors.NewInvalidValueError("SpecURL", errors.ErrEmptyValue)
+		return sdkerrors.NewInvalidValueError("SpecURL", sdkerrors.ErrEmptyValue)
 	}
 
 	if strings.TrimSpace(cfg.ScriptURL) == "" {
-		return errors.NewInvalidValueError("ScriptURL", errors.ErrEmptyValue)
+		return sdkerrors.NewInvalidValueError("ScriptURL", sdkerrors.ErrEmptyValue)
 	}
 
 	if strings.TrimSpace(cfg.PresetScriptURL) == "" {
-		return errors.NewInvalidValueError("PresetScriptURL", errors.ErrEmptyValue)
+		return sdkerrors.NewInvalidValueError("PresetScriptURL", sdkerrors.ErrEmptyValue)
 	}
 
 	if strings.TrimSpace(cfg.StylesURL) == "" {
-		return errors.NewInvalidValueError("StylesURL", errors.ErrEmptyValue)
+		return sdkerrors.NewInvalidValueError("StylesURL", sdkerrors.ErrEmptyValue)
 	}
 
 	if strings.TrimSpace(cfg.Favicon32) == "" {
-		return errors.NewInvalidValueError("Favicon32", errors.ErrEmptyValue)
+		return sdkerrors.NewInvalidValueError("Favicon32", sdkerrors.ErrEmptyValue)
 	}
 
 	if strings.TrimSpace(cfg.Favicon16) == "" {
-		return errors.NewInvalidValueError("Favicon16", errors.ErrEmptyValue)
+		return sdkerrors.NewInvalidValueError("Favicon16", sdkerrors.ErrEmptyValue)
 	}
 
 	if strings.TrimSpace(cfg.Template) == "" {
-		return errors.NewInvalidValueError("Template", errors.ErrEmptyValue)
+		return sdkerrors.NewInvalidValueError("Template", sdkerrors.ErrEmptyValue)
 	}
 
 	return nil
