bb87e67ceb41122c28a9b173beb7dd47ba22e591
Aliaksandr Babai <aliaksandr.babai@gmail.com>
2024-02-08 18:15:07 +0100

    refactor(errors): improve messages

--- errors/value.go
+++ errors/value.go
@@ -13,10 +13,10 @@ var (
 
 // NewRequiredValueError returns an error about missing required value for the given key.
 func NewRequiredValueError(key string) error {
-	return fmt.Errorf("'%s' is required", key)
+	return fmt.Errorf("%q is required", key)
 }
 
 // NewInvalidValueError returns an invalid value error for the given key with the cause.
 func NewInvalidValueError(key string, cause error) error {
-	return fmt.Errorf("'%s' has invalid value: %w", key, cause)
+	return fmt.Errorf("%q is invalid: %w", key, cause)
 }
